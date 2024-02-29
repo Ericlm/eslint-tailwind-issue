1329f6cc296288bf8a67ea4ff1569c01002a4de4
Éric Le Maître <eric.erwan.le.maitre@gmail.com>
2024-02-29 16:17:51 +0100

    ➕ Eslint tailwind plugin

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+
+const is = true
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="bg-red-600 size-16" src="./assets/logo.svg" />
+    <img alt="Vue logo" class="size-16 bg-red-600" src="./assets/logo.svg" :class="[{'text-yala': is}]" />
 
-    <div class="text-yolo">
+    <div class="text-yolo" :class="[is ? 'text-yulu' : 'text-red-50']">
       <HelloWorld msg="You did it!" />
     </div>
   </header>
